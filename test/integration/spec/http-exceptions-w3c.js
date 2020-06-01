58f69203989db0ce8e7998fa7e092c3babb85355
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-05-31 19:33:37 -0700

    ci: update to latest eslint config

--- test/integration/spec/http-exceptions-w3c.js
+++ test/integration/spec/http-exceptions-w3c.js
@@ -2,7 +2,6 @@
 
 /* eslint-env mocha */
 /* eslint-disable require-jsdoc */
-/* eslint-disable max-nested-callbacks */
 
 const {assert} = require('chai');
 
@@ -30,114 +29,39 @@ const {
   BadGateway,
   ServiceUnavailable,
   GatewayTimeout,
-  HttpVersionNotSupported
+  HttpVersionNotSupported,
   // This is targeting built code, so it will usually fail lint
   // eslint-disable-next-line import/no-unresolved
 } = require('../../../');
 
 [
-  [
-    BadRequest,
-    400
-  ],
-  [
-    Unauthorized,
-    401
-  ],
-  [
-    PaymentRequired,
-    402
-  ],
-  [
-    Forbidden,
-    403
-  ],
-  [
-    NotFound,
-    404
-  ],
-  [
-    MethodNotAllowed,
-    405
-  ],
-  [
-    NotAcceptable,
-    406
-  ],
-  [
-    ProxyAuthenticationRequired,
-    407
-  ],
-  [
-    RequestTimeout,
-    408
-  ],
-  [
-    Conflict,
-    409
-  ],
-  [
-    Gone,
-    410
-  ],
-  [
-    LengthRequired,
-    411
-  ],
-  [
-    PreconditionFailed,
-    412
-  ],
-  [
-    RequestEntityTooLarge,
-    413
-  ],
-  [
-    RequestUriTooLong,
-    414
-  ],
-  [
-    UnsupportedMediaType,
-    415
-  ],
-  [
-    RequestRangeNotSatisfiable,
-    416
-  ],
-  [
-    ExpectationFailed,
-    417
-  ],
-  [
-    InternalServerError,
-    500
-  ],
-  [
-    NotImplemented,
-    501
-  ],
-  [
-    BadGateway,
-    502
-  ],
-  [
-    ServiceUnavailable,
-    503
-  ],
-  [
-    GatewayTimeout,
-    504
-  ],
-  [
-    HttpVersionNotSupported,
-    505
-  ]
+  [BadRequest, 400],
+  [Unauthorized, 401],
+  [PaymentRequired, 402],
+  [Forbidden, 403],
+  [NotFound, 404],
+  [MethodNotAllowed, 405],
+  [NotAcceptable, 406],
+  [ProxyAuthenticationRequired, 407],
+  [RequestTimeout, 408],
+  [Conflict, 409],
+  [Gone, 410],
+  [LengthRequired, 411],
+  [PreconditionFailed, 412],
+  [RequestEntityTooLarge, 413],
+  [RequestUriTooLong, 414],
+  [UnsupportedMediaType, 415],
+  [RequestRangeNotSatisfiable, 416],
+  [ExpectationFailed, 417],
+  [InternalServerError, 500],
+  [NotImplemented, 501],
+  [BadGateway, 502],
+  [ServiceUnavailable, 503],
+  [GatewayTimeout, 504],
+  [HttpVersionNotSupported, 505],
 ].forEach(verifyCode);
 
-function verifyCode([
-  Constructor,
-  code
-]) {
+function verifyCode([Constructor, code]) {
   describe(Constructor.name, () => {
     describe('#code', () => {
       it('is defined', () => {
@@ -147,3 +71,5 @@ function verifyCode([
     });
   });
 }
+
+/* eslint-enable require-jsdoc */
